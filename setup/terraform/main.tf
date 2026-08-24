162f9a01c8c95710b5b8caf81bee49d46a44ed75
sairam5686 <jrsairam5686@gmail.com>
2026-08-24 12:12:27 +0530

    --commit fib

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -316,15 +316,7 @@ resource "aws_iam_user" "github_action_user" {
   name = "github-action-user"
 }
 
-resource "aws_iam_user_policy" "github_action_user_permission" {
-  user   = aws_iam_user.github_action_user.name
-  policy = data.aws_iam_policy_document.github_policy.json
-}
-
-data "aws_iam_policy_document" "github_policy" {
-  statement {
-    effect    = "Allow"
-    actions   = ["ecr:*", "eks:*", "ec2:*", "iam:GetUser"]
-    resources = ["*"]
-  }
+resource "aws_iam_user_policy_attachment" "github_action_user_admin" {
+  user       = aws_iam_user.github_action_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
