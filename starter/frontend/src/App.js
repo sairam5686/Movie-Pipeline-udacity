6bf6d158fac93792f6f63da4e5273c6b694168ad
sairam5686 <jrsairam5686@gmail.com>
2026-08-24 13:29:30 +0530

    Trigger CD workflow

--- starter/frontend/src/App.js
+++ starter/frontend/src/App.js
@@ -4,6 +4,8 @@ import MovieDetails from './components/MovieDetails';
 import './App.css';
 
 export default function App() {
+
+  
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   const handleMovieClick = (movie) => {
